81d6a0a0ee0363f6a8a060b55e1297f89286c7eb
Isaac Scheinfeld <ischeinfeld@outlook.com>
2015-05-12 17:30:35 -0400

    finished estimating parameters q(s|u,v) and e(x|s)

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from preprocessing import import_wsj, replace_rarities
-from parameters import sx_counts, uvs_counts, uv_counts, s_counts
+from parameters import sx_counts, uvs_counts, uv_counts, s_counts, q, e
 
 sentences = import_wsj("train")
 #print(sentences[0])
@@ -23,12 +23,22 @@ print()
 print("c(s,x) -> c(DT, the): ", sx_counts['DT']['the'])
 
 print()
-print("c(u,v,s) -> c(<!>, NN, NNS): ", uvs_counts['<!>']['NN']['NNS'])
+print("c(u,v,s) -> c(<START>, NN, NNS): ", uvs_counts['<START>']['NN']['NNS'])
 
 print()
 print("c(s) -> c(DT): ", s_counts['DT'])
 
 print()
-print("c(u,v) -> c(<!>, NN): ", uv_counts['<!>']['NN'])
+print("c(u,v) -> c(<START>, NN): ", uv_counts['<START>']['NN'])
 
+print()
+print()
+
+q_suv = q(uvs_counts, uv_counts, "NNS", "<START>", "NN")
+e_xs = e(sx_counts, s_counts, "the", "DT")
+
+print("q(s|u,v) -> q(NNS, <START>, NN): ", q_suv)
+print()
+
+print("e(x|s) -> e(the, DT): ", e_xs)
 print()
